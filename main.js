9b1d335cf7e5f6d89029b359cb6494fc65ac888c
chrisFederico <cristiano.federico.92@gmail.com>
2025-08-03 14:36:36 +0200

    split routes in separate file

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 const express = require('express');
-const { extractGenericArticle } = require('./functions');
+const routes = require('./routes');
 
 const app = express();
 const port = 3000;
 
-app.get('/extract', async (req, res) => {
-  const url = req.query.url;
-  if (!url) {
-    return res.status(400).send('Parametro URL mancante');
-  }
-
-  const html = await extractGenericArticle(url);
-  res.set('Content-Type', 'text/html');
-  res.send(html);
-});
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`API in ascolto su http://localhost:${port}`);
+  console.log(`Server attivo su http://localhost:${port}`);
 });
